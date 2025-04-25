b57ecad3df24b6e4e178765f7a8253cd08cc0a59
sudiary <164376280+sudiary@users.noreply.github.com>
2025-04-26 00:32:53 +0300

    schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -38,11 +38,13 @@ CREATE TABLE IF NOT EXISTS product (
     product_id SERIAL PRIMARY KEY,
     product_name VARCHAR(50),
     description TEXT,
-    price FLOAT,  -- Fiyatı FLOAT olarak değiştirdik
+    price FLOAT,
     category_id INT,
-    stock INT,  -- Stok miktarını INT olarak değiştirdik
+    stock INT,
+    cover_image_url TEXT, -- 📸 Kapak fotoğrafı
     FOREIGN KEY (category_id) REFERENCES category_table(category_id) ON DELETE CASCADE
 );
+
 
 -- 6️⃣ Siparişler
 CREATE TABLE IF NOT EXISTS order_tbl (
@@ -84,5 +86,12 @@ CREATE TABLE IF NOT EXISTS rate_tbl (
     product_id INT,
     rate FLOAT,  -- Puanlama değeri FLOAT olarak değiştirildi
     FOREIGN KEY (user_id) REFERENCES user_tbl(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(product_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS product_images (
+    image_id SERIAL PRIMARY KEY,
+    product_id INT NOT NULL,
+    image_url TEXT NOT NULL,
     FOREIGN KEY (product_id) REFERENCES product(product_id) ON DELETE CASCADE
 );
